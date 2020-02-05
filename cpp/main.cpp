022bc5aedd580d3717a2d7bb0524fa29e40ab901
Johan Lundquist <jolundq@gmail.com>
2020-02-05 13:19:32 +0100

    Initial commit

---
+++ cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <emscripten/emscripten.h>
+#include <fftw3.h>
+
+using namespace std;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double EMSCRIPTEN_KEEPALIVE process(float* data, int size, int nffts) {
+    cout << "PROCESSING" << endl;
+    return 5.0;
+}
+
+int main(int argc, char ** argv) {
+    fftw_complex in[10];
+    cout << "PROCESSINGGG" << endl;
+}
+
+#ifdef __cplusplus
+}
+#endif
